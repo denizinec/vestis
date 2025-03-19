bc5a6170a5988bd66b34ba86aa84cd2dde0b734e
Deniz İneç <denizinec9@gmail.com>
2025-03-20 00:45:33 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost'],
-
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      }
+    ],
   },
 };
 
