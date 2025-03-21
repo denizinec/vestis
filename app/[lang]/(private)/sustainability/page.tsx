2a75132b9a4916fa2dc7103d21cd715bf90618fc
Deniz İneç <denizinec9@gmail.com>
2025-03-22 02:40:00 +0300

    x

--- app/[lang]/(private)/sustainability/page.tsx
+++ app/[lang]/(private)/sustainability/page.tsx
@@ -1,11 +1,16 @@
 import Sustainability from '@/components/sustainability';
-import ProductsSection from '@/components/products-section';
-import ContactCta from '@/components/contact-cta';
+import Image from 'next/image';
 
 export default function Page() {
   return (
+    <>
     <div className="flex flex-col min-h-screen pt-40">
       <Sustainability />
+      <div className='w-full flex justify-center items-center'>
+        <Image src="/images/sustainability/sustainability.png" alt="Sustainability" width={2000} height={2000} />
     </div>
+    </div>
+      
+    </>
   );
 }
