e98ed4d1546b4fffb9eaead9e17ed6bffb8bac06
Deniz İneç <denizinec9@gmail.com>
2025-03-24 23:40:42 +0300

    added not-found wip

---
+++ app/[lang]/(private)/not-found.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Link from "next/link"
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">404</h1>
+      <p className="text-gray-500">Page not found</p>
+      <Link href={`/home`} className="text-blue-500">Go back to home</Link>
+    </div>
+  )
+}
